61e1283c306677c7751ab393e7709ae50b193520
David Henriques <23697@stu.ipbeja.pt>
2023-01-17 23:32:46 +0000

    Finished Menu

--- main.py
+++ main.py
@@ -7,72 +7,246 @@
 #             Jo�o Tavanez (3109)               
 #                                               
 
-#from classes.cesar import encrypt, decrypt, bruteForce
-#from classes.viginere import encrypt, decrypt, bruteForce
-#from classes.playfair import encrypt, decrypt, bruteForce
-#from classes.rail_fence import encrypt, decrypt, bruteForce
-#from classes.affine import encrypt, decrypt, bruteForce
+from classes.cesar import Cesar
 import os
+import time
 
-#Limpar Ecra Windows/Linux
+# Clear Screen Windows/Linux
 def clearScreen():
     os.system('cls' if os.name=='nt' else 'clear')
 
-mensagem = "Nos Somos do BENFICA - O Melhor!"
-key = 9
+message = "herois do mar nobre povo"
+key = 35
+mainChoice = ""
+subChoice = ""
 
-#Menu
-print("MENU DE TIPO DE CIFRA")
-print("1: Cesar")
-print("2: Viginere")
-print("3: Playfair")
-print("4: Rail-Fence")
-print("5: Affine")
-print("6: Metodo Kasiski")
-print("")
-print("Prima X Para Sair")
-choice = input("Por favor selecione uma opcao ")
 
-match choice:
-    case "1":
-        clearScreen()
-        print("Cifra de Cesar")
-        print("Mensagem:", mensagem)
-        print("Chave", key)
-
-        print("Escolha a opcao:")
-        print("1: Cifrar")
-        print("2: Decifrar")
-        print("3: Quebrar a Cifra")
-        opcao = input("Por favor selecione uma opcao ")
-
-        match opcao:
-            case "1":
-                cesar.cifrar()
-            case "2":
-                cesar.decifrar()
-            case "3":
-                cesar.bruto()
-            case _:
-                print("Opcao Invalida ")             
-            
-    case "2":
-        print("Viginere")
-
-    case "3":
-        print("Playfair")
-
-    case "4":
-        print("Rail-Fence")
-
-    case "5":
-        print("Affine")
+# Menu
+while mainChoice.lower() != "x":
+    clearScreen()
+    print("MENU DE TIPO DE CIFRA")
+    print("")
+    print("1: Cesar")
+    print("2: Viginere")
+    print("3: Playfair")
+    print("4: Rail-Fence")
+    print("5: Affine")
+    print("6: Metodo Kasiski")
+    print("")
+    print("Prima X Para Sair")
+    print("")
+    mainChoice = input("Por favor selecione uma opcao ")
     
-    case "6":
-        kasisky.comprimento()
-        kasisky.decifrar()
-    
-    case "x":
-        print("Adeus")
-    case _:
-        print("Opcao Invalida ")
+    match mainChoice.lower():
+        
+        # Cesar Cypher
+        case "1":   
+
+            while subChoice.lower() != "X":
+                clearScreen()
+                print("Cifra de Cesar")
+                print("")
+                print("Escolha a opcao:")
+                print("1: Cifrar")
+                print("2: Decifrar")
+                print("3: Quebrar a Cifra")
+                print("")
+                print("Prima X Para Sair")
+                print("")
+                subChoice = input("Por favor selecione uma opcao ")
+                print("")
+                match subChoice.lower():
+                    
+                    case "1":
+                        msgKeyCesar = Cesar(message,key)
+                        encrypt = msgKeyCesar.encrypt(message,key)
+                        print(encrypt)
+                        time.sleep(3)
+
+                    case "2":
+                        msgKeyCesar = Cesar(message,key)
+                        cryptogram = msgKeyCesar.encrypt(message,key)
+                        print(cryptogram)
+                        print("")
+                        decrypt = msgKeyCesar.decrypt(cryptogram,key)
+                        print(decrypt)
+                        time.sleep(3)
+                    
+                    case "3":
+                        print("cesar.bruto()")
+                    
+                    case "x":
+                        break
+                    
+                    case _:
+                        print("Opcao Invalida ")
+                        time.sleep(2)             
+        
+        # Viginere Cypher        
+        case "2":
+
+            while subChoice.lower() != "X":
+                            clearScreen()
+                            print("Cifra de Viginere")
+                            print("")
+                            print("Escolha a opcao:")
+                            print("1: Cifrar")
+                            print("2: Decifrar")
+                            print("3: Quebrar a Cifra")
+                            print("")
+                            print("Prima X Para Sair")
+                            print("")
+                            subChoice = input("Por favor selecione uma opcao ")
+                            print("")
+                            match subChoice.lower():
+                                
+                                case "1":
+                                    print("viginere.cifrar()")
+                                
+                                case "2":
+                                    print("viginere.decifrar()")
+                                
+                                case "3":
+                                    print("viginere.bruto()")
+                                
+                                case "x":
+                                    break
+                                
+                                case _:
+                                    print("Opcao Invalida ")
+                                    time.sleep(2)         
+
+        # Playfair Cypher
+        case "3":
+
+             while subChoice.lower() != "X":
+                clearScreen()
+                print("Cifra de Playfair")
+                print("")
+                print("Escolha a opcao:")
+                print("1: Cifrar")
+                print("2: Decifrar")
+                print("3: Quebrar a Cifra")
+                print("")
+                print("Prima X Para Sair")
+                print("")
+                subChoice = input("Por favor selecione uma opcao ")
+                print("")
+                match subChoice.lower():
+                    
+                    case "1":
+                        print("Playfair.cifrar()")
+                    
+                    case "2":
+                        print("Playfair.decifrar()")
+                    
+                    case "3":
+                        print("Playfair.bruto()")
+                    
+                    case "x":
+                        break
+                    
+                    case _:
+                        print("Opcao Invalida ") 
+                        time.sleep(2)  
+
+        # Rail-Fence Cypher          
+        case "4":
+             while subChoice.lower() != "X":
+                clearScreen()
+                print("Cifra de Rail-Fence")
+                print("")
+                print("Escolha a opcao:")
+                print("1: Cifrar")
+                print("2: Decifrar")
+                print("3: Quebrar a Cifra")
+                print("")
+                print("Prima X Para Sair")
+                print("")
+                subChoice = input("Por favor selecione uma opcao ")
+                print("")
+                match subChoice.lower():
+                    
+                    case "1":
+                        print("RailFence.cifrar()")
+                    
+                    case "2":
+                        print("RailFence.decifrar()")
+                    
+                    case "3":
+                        print("RailFence.bruto()")
+                    
+                    case "x":
+                        break
+                    
+                    case _:
+                        print("Opcao Invalida ") 
+                        time.sleep(2)  
+
+        # Affine Cypher          
+        case "5":
+             while subChoice.lower() != "X":
+                clearScreen()
+                print("Cifra de Affine")
+                print("")
+                print("Escolha a opcao:")
+                print("1: Cifrar")
+                print("2: Decifrar")
+                print("3: Quebrar a Cifra")
+                print("")
+                print("Prima X Para Sair")
+                print("")
+                subChoice = input("Por favor selecione uma opcao ")
+                print("")
+                match subChoice.lower():
+                    
+                    case "1":
+                        print("Affine.cifrar()")
+                    
+                    case "2":
+                        print("Affine.decifrar()")
+                    
+                    case "3":
+                        print("Affine.bruto()")
+                    
+                    case "x":
+                        break
+                    
+                    case _:
+                        print("Opcao Invalida ") 
+                        time.sleep(2)     
+
+        # Kasiski Method on Viginere Cypher      
+        case "6":
+             while subChoice.lower() != "X":
+                clearScreen()
+                print("Kasiski Method")
+                print("")
+                print("Escolha a opcao:")
+                print("1: Comprimento")
+                print("2: Decifrar")
+                print("")
+                print("Prima X Para Sair")
+                print("")
+                subChoice = input("Por favor selecione uma opcao ")
+                print("")
+                match subChoice.lower():
+                    
+                    case "1":
+                        print("comprimento")
+                    
+                    case "2":
+                        print("decifrar")
+                    
+                    case "x":
+                        break
+                    
+                    case _:
+                        print("Opcao Invalida ")
+                        time.sleep(2)             
+
+        case "x":
+            print("Adeus")
+            break
+        case _:
+            print("Opcao Invalida ")
